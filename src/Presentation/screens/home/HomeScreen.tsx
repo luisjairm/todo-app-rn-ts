6bc4094d23f7c2ca50b8c14d3f3570eb26330afc
luisjairm <luisjairm.dev@gmail.com>
2023-09-24 14:19:03 -0600

    fix: :bug: navigation

--- src/Presentation/screens/home/HomeScreen.tsx
+++ src/Presentation/screens/home/HomeScreen.tsx
@@ -2,18 +2,29 @@ import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import GlobalStyles from '../../theme/GlobalStyles'
 import { ThemeApp } from '../../theme/AppTheme'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { TasksNavigationStackParamList } from '../../navigations/TasksNavigationStack'
 
-const HomeScreen = () => {
+interface Props extends NativeStackScreenProps<TasksNavigationStackParamList> {}
+
+const HomeScreen = ({ navigation, route }: Props) => {
   return (
     <View style={GlobalStyles.containerScreen}>
       <ScrollView style={{ width: '100%' }}>
         <Text style={{ fontSize: 18, color: ThemeApp.WHITE, fontWeight: 'bold' }}>Bienvenido Luis</Text>
 
       </ScrollView>
-      <TouchableOpacity style={{ position: 'absolute', bottom: 20, right: 20 }}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate('FormTaskScreen')}
+        style={
+        [
+          GlobalStyles.shadow,
+          { position: 'absolute', bottom: 20, right: 20 }
+        ]
+}
+      >
         <Icon
           name='plus-circle'
-          onPress={() => console.log('1')}
           size={60}
           color={ThemeApp.FUCHSIA}
         />
