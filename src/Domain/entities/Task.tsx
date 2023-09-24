bd30e6fc6a1fb0db28fdf815368077d4edf5b45a
luisjairm <luisjairm.dev@gmail.com>
2023-09-24 14:17:31 -0600

    feat: :sparkles: Campos para las tareas

---
+++ src/Domain/entities/Task.tsx
@@ -0,0 +1,16 @@
+export interface Task {
+  id: string
+  name: string
+  description: string
+  category: TaskCategories
+  status: boolean
+  deadline: string
+  createdAt: string
+  updatedAt: string
+}
+
+export enum TaskCategories {
+  SCHOOL = 'school',
+  PERSONAL = 'staff',
+  WORK = 'work'
+}
