bcb510787eaa31bff15526ed3576c301d370c5f6
luisjairm <luisjairm.dev@gmail.com>
2023-09-26 12:21:00 -0600

    style: :lipstick: Tarjeta para mostra la información de cada tarea

---
+++ src/Presentation/components/TaskCard.tsx
@@ -0,0 +1,76 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
+import { Task, TaskCategories } from '../../Domain/entities/Task'
+import { ThemeApp } from '../theme/AppTheme'
+import { dateFormat } from '../utils/Helpers'
+import { CategoryTaksSpanish } from '../utils/Translations'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+interface Props {
+  task: Task
+}
+
+const selectCheckColor = (category: TaskCategories) => {
+  const colors: Record<TaskCategories, string> = {
+    [TaskCategories.PERSONAL]: ThemeApp.SKY_BLUE,
+    [TaskCategories.WORK]: ThemeApp.WORK,
+    [TaskCategories.SCHOOL]: ThemeApp.SCHOOL
+  }
+  return colors[category]
+}
+
+const TaskCard = ({ task }: Props) => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { name, description, createdAt, category, is_completed, id } = task
+
+  return (
+    <View
+      style={{
+        width: '90%',
+        backgroundColor: ThemeApp.DEEP_NAVY,
+        margin: 5,
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+      }}
+    >
+      <View style={{ width: '20%' }}>
+        <BouncyCheckbox
+          size={40}
+          fillColor={selectCheckColor(category)}
+          unfillColor={ThemeApp.DEEP_NAVY}
+          disableText
+          isChecked={is_completed}
+        // text='Custom Checkbox'
+          iconStyle={{ borderColor: ThemeApp.DEEP_NAVY }}
+          innerIconStyle={{ borderWidth: 2 }}
+          onPress={(isChecked: boolean) => console.log(isChecked)}
+        />
+      </View>
+      <View style={{ width: '60%' }}>
+        <Text style={{ color: ThemeApp.WHITE, fontSize: 15, fontWeight: 'bold' }}>{name}</Text>
+        <Text style={{ color: ThemeApp.LIGHT_GRAY, fontSize: 13 }}>{description}</Text>
+        <Text style={{ color: ThemeApp.LIGHT_GRAY, fontSize: 11 }}>{dateFormat(createdAt)}</Text>
+        <Text style={{ color: ThemeApp.LIGHT_GRAY, fontSize: 11 }}>{CategoryTaksSpanish[category]}</Text>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => console.log(is_completed)}
+        style={
+        [
+          { width: '10%' }
+        ]
+}
+      >
+        <Icon
+          name='pencil'
+          size={25}
+          color={selectCheckColor(category)}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default TaskCard
