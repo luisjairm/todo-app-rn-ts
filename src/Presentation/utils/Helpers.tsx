6c286b2f42711c69df95ce06658f368169473779
luisjairm <luisjairm.dev@gmail.com>
2023-09-26 12:19:40 -0600

    feat: :sparkles: funcion para formatear fecha

--- src/Presentation/utils/Helpers.tsx
+++ src/Presentation/utils/Helpers.tsx
@@ -8,3 +8,10 @@ export const dateFormatToSql = (date: string) => {
   const newDate = moment(date).format('YYYY-MM-DD HH:mm:ss')
   return newDate
 }
+
+export const dateFormat = (date: string | undefined) => {
+  if (date === undefined || date.length < 1) return undefined
+  const newDate = moment(date).format('L')
+  const newTime = moment(date).format('LT')
+  return `${newDate} ${newTime}`
+}
